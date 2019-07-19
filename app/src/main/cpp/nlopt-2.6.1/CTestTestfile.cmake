d4083fb61160a6a99bf24d01bc24163bef0b4dad
quyenpham173 <quyenpham173@gmail.com>
2019-07-19 16:18:00 +0700

    ...

--- app/src/main/cpp/nlopt-2.6.1/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/superman/Do_An/Offical/preprocess/nlopt-2.6.1
-# Build directory: /home/superman/Do_An/Offical/preprocess/nlopt-2.6.1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/api")
-subdirs("test")
